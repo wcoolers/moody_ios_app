145541c5a0cb04812ba6041f56f168ffcc545d46
Adegoke <devv.online@gmail.com>
2025-04-16 13:00:23 -0400

    Initial Commit

---
+++ adegokeakanbi_a2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  adegokeakanbi_a2
+//
+//  Created by Adegoke on 2025-04-16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
